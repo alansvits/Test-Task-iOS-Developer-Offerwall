fe3850311d4f96dd81081a522badeb39d4252005
Stas <ssffxixxc@outlook.com>
2020-01-12 10:20:18 +0200

    Initial Commit

---
+++ Test Task @ iOS Developer | Offerwall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Task @ iOS Developer | Offerwall
+//
+//  Created by Stas Shetko on 2/01/20.
+//  Copyright © 2020 Stas Shetko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
